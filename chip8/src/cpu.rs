50047089b5c43a9478f53b45ee7ca449444a49f0
Taylor Fahlman <tfahlman@redhat.com>
2023-10-25 13:11:18 -0700

    Added 0x0nnn through 0x6nnn instructions

--- chip8/src/cpu.rs
+++ chip8/src/cpu.rs
@@ -4,7 +4,7 @@ pub struct CPU {
     mem: [u8; 4096], // Memory
     v: [u8; 16], // 16 Registers, V0-VF
     stack: [u16; 16], // Stack, 16 16-bits
-    sp: u8, // Stack Pointer
+    sp: usize, // Stack Pointer
     dt: u8 // Delay Timer
 }
 
@@ -20,7 +20,6 @@ impl CPU {
             mem: [0;4096],
             dt: 0,
         }
-
     }
 
     pub fn load_memory(&mut self, data: &[u8]) {
@@ -40,12 +39,81 @@ impl CPU {
         // The first instruction is bitshifted to the left to fill the first 8 bits
         // Then OR-ed with the second instruction to fill the second 8 bits
         let opcode = (self.mem[self.pc] as u16) << 8 | (self.mem[self.pc + 1] as u16);
-
         self.execute_opcode(opcode);
     }
 
     fn execute_opcode(&mut self, opcode: u16) {
-        println!("{}", opcode);
-        self.pc += 2;
+        match opcode & 0xF000 {
+            0x0000 => self.op_0nnn(opcode),
+            0x1000 => self.op_1nnn(opcode),
+            0x2000 => self.op_2nnn(opcode),
+            0x3000 => self.op_3xnn(opcode),
+            0x4000 => self.op_4xnn(opcode),
+            0x5000 => self.op_5xy0(opcode),
+            0x6000 => self.op_6xxx(opcode),
+            _ => println!("Unimplemented opcode: {:X}", opcode)
+        }
+    }
+
+    fn op_0nnn(&mut self, opcode: u16) {
+        match opcode & 0x00FF {
+            0x00E0 => println!("Clear screen"),
+            0x00EE => println!("Move stack pointer"),
+            _ => println!("Unimplemented opcode: {:X}", opcode)
+        }
+    }
+
+    // 1nnn - JP addr
+    //  The interpreter sets the program counter to nnn.
+    fn op_1nnn(&mut self, opcode: u16) {
+        let nnn = ((opcode & 0x0FFF)) as usize;
+        self.pc = nnn
+    }
+
+    // 2nnn - CALL addr
+    //  The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
+    fn op_2nnn(&mut self, opcode: u16) {
+        self.stack[self.sp] = self.pc as u16;
+        self.sp += 1;
+        let nnn = ((opcode & 0x0FFF)) as usize;
+        self.pc = nnn;   
+    }
+
+    // 3xnn - SE Vx, nn
+    // The interpreter compares register Vx to nn, and if they are equal, increments the program counter by 2, thus skipping the next instruction
+    fn op_3xnn(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let nn = (opcode & 0x00FF) as u8;
+        if self.v[x] == nn {
+            self.pc += 2;
+        }
+    }
+
+    // 4xnn - SNE Vx, byte
+    // The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
+    fn op_4xnn(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let nn = (opcode & 0x00FF) as u8;
+        if self.v[x] != nn {
+            self.pc += 2;
+        }
+    }
+
+    // 5xy0 - SE Vx, Vy
+    //  The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
+    fn op_5xy0(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = (opcode & 0x00F0) as usize;
+        if self.v[x] != self.v[y] {
+            self.pc += 2;
+        }
+    }
+    
+    // 6xnn - LD Vx, byte
+    // Set Vx = nn.
+    fn op_6xxx(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let nn = (opcode & 0x00FF) as u8;
+        self.v[x] = nn;
     }
 }
